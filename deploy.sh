82e14c7f3ec991ec68f1f0b510882cb7a218b063
Jatin Joshi <imcybergeek@gmail.com>
2022-08-19 17:21:02 +0530

    server downtime

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@ cd /home/ubuntu/server
 echo "pulling updated code"
 git pull
 echo "stopping server"
+SECONDS=0
 docker stop server
 echo "removing server"
 docker rm server
@@ -11,4 +12,6 @@ echo "building new image"
 docker build . -t server
 echo "running container"
 docker run -d -p 5000:5000 --name server server
+ELAPSED="Server Downtime: $(($SECONDS / 3600))hrs $((($SECONDS / 60) % 60))min $(($SECONDS % 60))sec"
+echo $ELAPSED
 echo "deployed"
--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-let http = require('http');
 let crypto = require('crypto');
 const exec = require('child_process').exec;
 
@@ -12,7 +11,12 @@ app.post('/hook', (req, res) => {
         let sig = "sha1=" + crypto.createHmac('sha1', secret).update(chunk.toString()).digest('hex');
 
         if (req.headers['x-hub-signature'] == sig) {
-            exec('bash deploy.sh', (err, stdout, stderr) => console.log(err, stdout, stderr));
+            const process = exec('bash deploy.sh', (err, stdout, stderr) => console.log(err, stdout, stderr));
+
+            process.on('message', () => {
+                
+            })
+
             console.log('Done!');
             res.status(200);
         }
