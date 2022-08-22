d9057350fe2d6369a96d3adcf6e101e30ea56a90
Jatin Joshi <imcybergeek@gmail.com>
2022-08-22 15:57:38 +0530

    peace

--- index.js
+++ index.js
@@ -11,18 +11,14 @@ const downtimeMailer = (stdout) => mailer(stdout.split('Server Downtime: ')[1]).
 const mailer = async (downtime) => {
 // Generate test SMTP service account from ethereal.email
   // Only needed if you don't have a real mail account for testing
-  let testAccount = await nodemailer.createTestAccount();
-
-  // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    host: "smtp.ethereal.email",
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
     port: 587,
-    secure: false, // true for 465, false for other ports
     auth: {
-      user: testAccount.user, // generated ethereal user
-      pass: testAccount.pass, // generated ethereal password
-    },
-  });
+        user: 'elisabeth.ullrich4@ethereal.email',
+        pass: 'cmq8KNYpvwNGnzyfVJ'
+    }
+});
 
   // send mail with defined transport object
   let info = await transporter.sendMail({
