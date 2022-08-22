39cd997fb522ee001293b8b178f7c20830089204
Jatin Joshi <imcybergeek@gmail.com>
2022-08-22 10:35:14 +0530

    Downtime counter added

--- index.js
+++ index.js
@@ -5,19 +5,16 @@ const exec = require('child_process').exec;
 const app = express();
 
 const secret = "secret";
+let downtime;
 
 app.post('/hook', (req, res) => {
     req.on('data', function (chunk) {
         let sig = "sha1=" + crypto.createHmac('sha1', secret).update(chunk.toString()).digest('hex');
 
         if (req.headers['x-hub-signature'] == sig) {
-            const process = exec('bash deploy.sh', (err, stdout, stderr) => console.log(err, stdout, stderr));
+            const process = exec('bash deploy.sh', (err, stdout, stderr) => {console.log(err,stdout,stderr); downtime = stdout});
 
-            process.on('message', () => {
-                
-            })
-
-            console.log('Done!');
+            console.log(stdout);
             res.status(200);
         }
     })
