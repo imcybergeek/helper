db8741e2e3b503a2e796fba4fc3e6908b96fbb0e
Jatin Joshi <imcybergeek@gmail.com>
2022-08-22 13:16:33 +0530

    asdfghjk

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const exec = require('child_process').exec;
 const app = express();
 
 const secret = "secret";
-const callback = (stdout) => console.log(stdout)
+const downtime = (stdout) => console.log("asdfghjk",stdout)
 
 app.post('/hook', (req, res) => {
     req.on('data', function (chunk) {
         let sig = "sha1=" + crypto.createHmac('sha1', secret).update(chunk.toString()).digest('hex');
 
         if (req.headers['x-hub-signature'] == sig) {
-            const process = exec('bash deploy.sh', (err, stdout, stderr) => callback(stdout))
+            const process = exec('bash deploy.sh', (err, stdout, stderr) => downtime(stdout))
             res.status(200);
         }
     })
